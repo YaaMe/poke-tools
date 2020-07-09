6d8972e52804a599fcde6ffd2b247224bfd6ba8d
yaame <yaame@debian>
2020-07-09 10:15:15 +0800

    switch js server => jsdelivr

---
+++ build/precache-manifest.481af6e46e20fb1cd44e48316bc5f9b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72ef41c772df5fbf8c0758809c7ad08c",
+    "url": "https://cdn.jsdelivr.net/gh/yaame/poke-tools@gh-pages/build/index.html"
+  },
+  {
+    "revision": "8146498e76a6ef8f02a1",
+    "url": "https://cdn.jsdelivr.net/gh/yaame/poke-tools@gh-pages/build/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6af8988843c46cc9051a",
+    "url": "https://cdn.jsdelivr.net/gh/yaame/poke-tools@gh-pages/build/static/css/main.ec7d6cc1.chunk.css"
+  },
+  {
+    "revision": "8146498e76a6ef8f02a1",
+    "url": "https://cdn.jsdelivr.net/gh/yaame/poke-tools@gh-pages/build/static/js/2.b1424504.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "https://cdn.jsdelivr.net/gh/yaame/poke-tools@gh-pages/build/static/js/2.b1424504.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6af8988843c46cc9051a",
+    "url": "https://cdn.jsdelivr.net/gh/yaame/poke-tools@gh-pages/build/static/js/main.465242d1.chunk.js"
+  },
+  {
+    "revision": "abf3c919d25f64487dfa",
+    "url": "https://cdn.jsdelivr.net/gh/yaame/poke-tools@gh-pages/build/static/js/runtime-main.24d77e1a.js"
+  }
+]);
