4520a50413b6b81cc5042d9031102e9d6cd4823a
Jason Cooper <master@deamwork.com>
2020-10-27 23:11:30 +0800

    help needed

--- src/containers/Group/Env/index.tsx
+++ src/containers/Group/Env/index.tsx
@@ -5,36 +5,58 @@ import BuildMember from "../BuildMember";
 
 interface PokemonRankingState {
     seasonID?: string;
-    seasons?: {
-        [id: string]: SeasonInfos;
-    };
+    seasons?: Season;
 }
 
 interface SeasonReducerAction {
     type: string;
     season: string;
+    seasons: Season;
 }
 
 export const PokemonRanking = () => {
     const seasonReducer = (state: PokemonRankingState, action: SeasonReducerAction) => {
         switch (action.type) {
             case 'setSeason':
-                return {...state, seasonID: action.season};
+                return {...state, seasonID: action.season, seasons: action.seasons};
         }
         return state;
     }
+    const initialSeasonInfo: SeasonInfo = {
+        name: '0', start: '0', end: '0', cnt: -1, rule: 0, season: -1, rst: -1, ts1: -1, ts2: -1, reg: '0'
+    }
+    const initialSeasonInfos: SeasonInfos = {
+        '10001': initialSeasonInfo
+    }
+    const initialSeason: Season = {
+        '0': initialSeasonInfos
+    }
+    const initialPokemonRankingState: PokemonRankingState = {
+        seasons: initialSeason,
+        seasonID: '0'
+    }
 
     const [ rom, setRom ] = useState<SeasonListReqBody>({soft: "Sw"}); // 版本，Sw 剑， Sh 盾
-    const [ season, seasonDispatcher ] = useReducer(seasonReducer, {} as PokemonRankingState); // 赛季选择，会下传子组件，内含带api请求
+    const [ season, seasonDispatcher ] = useReducer(seasonReducer, initialPokemonRankingState); // 赛季选择，会下传子组件，内含带api请求
     const [ mode, setMode ] = useState<string>('single'); // 单双打
     const [ view, setView ] = useState<string>('trainee'); // 视图：训练师；Pokemon
 
-    const dispatch = (seasonID: string) => {
+    const dispatch = (seasonID: string, seasons: Season) => {
         seasonDispatcher({
             type: 'setSeason',
             season: seasonID,
+            seasons,
         })
     };
+    const getSeasonInfo = (season: PokemonRankingState, mode: string) => {
+        console.log("called getSeasonInfo..", season)
+
+        console.log("called getSeasonInfo and pass check", season)
+        const modeNum = mode === 'single' ? 1 : 2;
+        const seasonID = `10${season.seasonID!}${modeNum}`;
+        //      1~11 赛季集合   11 特定赛季序号   10固定位  11 特定赛季序号  模式 rule+1，0→1 单打，1→2 双打   10111 赛季ID
+        return season.seasons![season.seasonID!][seasonID]
+    }
 
     return <div>
         <Container fluid={true}>
@@ -119,7 +141,9 @@ export const PokemonRanking = () => {
             </Row>
 
             <Row style={{paddingTop: 10}}>
-                data...
+                <Col md={{span: 10, offset: 1}}>
+                    <PokemonView season={getSeasonInfo(season, mode)}/>
+                </Col>
             </Row>
         </Container>
 
@@ -132,7 +156,7 @@ class SeasonSelectorProps {
     };
     rom?: SeasonListReqBody;
     selectedSeason?: string;
-    seasonDispatch!: (seasonID: string) => void;
+    seasonDispatch!: (seasonID: string, seasons: Season) => void;
 }
 
 const PokemonHomeSeasonSelector = (props: SeasonSelectorProps) => {
@@ -149,11 +173,25 @@ const PokemonHomeSeasonSelector = (props: SeasonSelectorProps) => {
         );
     }, []);
 
-    return <select disabled={!seasons} value={selectedSeason} onChange={(event => seasonDispatch(event.target.value))}>
+    return <select disabled={!seasons} value={selectedSeason} onChange={(event => seasonDispatch(event.target.value, seasons!))}>
         <option key='select' value='select'>{seasons ? 'Select season...' : 'Loading data...'}</option>
         {Object.keys(seasons ?? {}).reverse().map((v,i) => {
             const k = seasons![v][Object.keys(seasons![v]).pop()!]
             return <option key={i} value={v}>{k.name} {k.start} - {k.end}</option>
         })}
     </select>
+}
+
+class PokemonViewProps {
+    season?: SeasonInfo;
+}
+
+const PokemonView = (props: PokemonViewProps) => {
+    let {name, cnt, start, end, season} = props.season!;
+    return <div>
+        Season {season} Info <br/>
+        Season Name: {name} <br/>
+        Time: {start} ~ {end} <br/>
+        Total Player: {cnt} <br/>
+    </div>
 }
